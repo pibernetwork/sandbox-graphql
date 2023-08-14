14288d697b1cefab0522bb2ea6fb8fabb331866d
Mauricio Piber <mauriciopiber@gmail.com>
2023-08-14 18:36:24 -0300

    Added GraphQL Server

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,7 +1,53 @@
-interface TestA {
-  value: string;
-}
+import { ApolloServer } from '@apollo/server';
+import { expressMiddleware } from '@apollo/server/express4';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import express from 'express';
+import gql from 'graphql-tag';
+import http from 'http';
+import 'reflect-metadata';
 
-const a: TestA = { value: '1234' };
+dotenv.config();
 
-console.log(a);
+const typeDefs = gql.default`
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => 'world'
+  }
+};
+
+const { json } = bodyParser;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+const httpServer = http.createServer(app);
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  introspection: true
+});
+
+await server.start();
+
+app.get('/', (_, res) => {
+  res.status(301).redirect('/graphql');
+});
+
+app.use(
+  '/graphql',
+  cors<cors.CorsRequest>(),
+  json(),
+  expressMiddleware(server)
+);
+
+export default httpServer;
