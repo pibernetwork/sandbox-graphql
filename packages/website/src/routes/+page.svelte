a6ac5ff4c89ccfdc031d202f08f84e9b7779d314
Mauricio Piber <mauriciopiber@gmail.com>
2023-08-15 11:08:26 -0300

    Added houdini

--- packages/website/src/routes/+page.svelte
+++ packages/website/src/routes/+page.svelte
@@ -1,3 +1,11 @@
+<script lang="ts">
+  import type { PageData } from './$houdini';
+  export let data: PageData;
+
+  $: ({ MyQuery } = data);
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 <h1 class="m-4 text-3xl font-bold underline">Hello world!</h1>
+{$MyQuery.data?.hello}
